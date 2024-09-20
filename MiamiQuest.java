f4b08aa1255e94faf198df7a0bb92be01c200c4c
mohamedlemin22 <155915386+mohamedlemin22@users.noreply.github.com>
2024-09-20 10:54:11 -0400

    Update MiamiQuest.java

--- MiamiQuest.java
+++ MiamiQuest.java
@@ -1,9 +1,27 @@
 
 public class MiamiQuest {
 
+	public void displayHelpMenu() {
+    System.out.println("---- Help Menu ----");
+    System.out.println("Controls:");
+    System.out.println("- Type your answers and press Enter.");
+    System.out.println("- Type 'help' at any time to view this menu again.");
+    System.out.println("\nGame Rules:");
+    System.out.println("- Earn 10 credit points to graduate.");
+    System.out.println("- Pass classes by scoring at least 6 out of 10 on finals.");
+    System.out.println("- Bonuses and penalties affect your final exam score.");
+    System.out.println("\nTips:");
+    System.out.println("- Be quick on timed questions.");
+    System.out.println("- Pay attention to class attributes.");
+    System.out.println("--------------------");
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		displayHelpMenu();
 	}
+
+
+
 
 }
