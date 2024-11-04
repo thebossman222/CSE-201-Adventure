3942882ff13fbda312cfb7772ce6dddb547cbeaf
ellismp4 <ellismp4@miamioh.edu>
2024-11-03 19:21:54 -0500

    java doc comments

--- Professor.java
+++ Professor.java
@@ -1,10 +1,19 @@
 import java.util.Random;
 
+/**
+ * Represents a Professor with a difficulty level and extra credit 
+ * attributes that affect the gameplay in the MiamiQuest game.
+ */
 public class Professor {
 
     private String difficulty;
     private int extraCredit;
 
+    /**
+     * Randomizes the difficulty of the professor.
+     * 
+     * @return The difficulty level as a string, either "Easy" or "Hard".
+     */
     public String randomizeDifficulty() {
         Random ran = new Random();
         int diff = ran.nextInt(2); // Generates 0 or 1
@@ -16,11 +25,16 @@ public class Professor {
         return this.difficulty;
     }
 
+    /**
+     * Assigns extra credit or deduction points based on the difficulty level of the professor.
+     * 
+     * @return The amount of extra credit, which is 2 for "Easy" and -2 for "Hard".
+     */
     public int assignExtraCredit() {
         if (randomizeDifficulty().equalsIgnoreCase("Easy")) {
-            this.extraCredit = 2;
+            this.extraCredit = 2; // Positive extra credit for easy difficulty
         } else {
-            this.extraCredit = -2;
+            this.extraCredit = -2; // Negative extra credit for hard difficulty
         }
         return this.extraCredit;
     }
