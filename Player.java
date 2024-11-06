543fb7fb9b24dbfea06ae1453a9029f384d07e14
mohamedlemin22 <155915386+mohamedlemin22@users.noreply.github.com>
2024-11-06 10:40:08 -0500

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,27 @@
+Public Class Player {
+
+private int credit;
+private int failedExam;
+
+  Public player(){
+    
+  }
+
+  public Class registerClass(){
+    
+  }
+
+  public int checkCredit(){
+    
+  }
+
+  public boolean DropClass(){
+    
+  }
+
+  public Exam takeExam(){
+    
+  }
+
+  
+}
