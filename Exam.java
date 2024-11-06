4927e65e00662835b6078ca3b7874c89442d11a7
mohamedlemin22 <155915386+mohamedlemin22@users.noreply.github.com>
2024-11-06 10:46:37 -0500

    Create Exam.java

---
+++ Exam.java
@@ -0,0 +1,19 @@
+Public class Exam{
+
+  private List<> question;
+  private int counterDownTimer;
+  private int score;
+
+  public boolean startExam(){
+    
+  }
+
+  public boolean submitExam(){
+    
+  }
+  public int calculateScore(){
+    
+  }
+
+  
+}
