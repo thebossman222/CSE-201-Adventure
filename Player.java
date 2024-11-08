f7a43bba339ce8472cbcf8a3f66e7e93d20ee006
mohamedlemin22 <155915386+mohamedlemin22@users.noreply.github.com>
2024-11-07 23:36:34 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,11 +1,15 @@
 public class Player {
     private int credits;
     private int failedExams;
+    private boolean canDrop;
+    private boolean canRetake;
 
     // Constructor to initialize player with default values
     public Player() {
         this.credits = 0;
         this.failedExams = 0;
+        this.canDrop = true; // Player can drop a class initially
+        this.canRetake = true; // Player can retake an exam initially
     }
 
     // Registers the player for a new class
@@ -19,20 +23,44 @@ public class Player {
         System.out.println("You currently have " + credits + " credits.");
         return credits;
     }
+    
+    // Checks if the player can drop a class
+    public boolean canDropClass() {
+        return canDrop;
+    }
 
-    // Allows the player to drop a class
+    // Allows the player to drop a class if possible
     public boolean dropClass() {
-        // Example logic to drop a class
-        System.out.println("Class dropped.");
-        return true; // Return true if successful
+        if (canDrop) {
+            canDrop = false; // Set canDrop to false once used
+            System.out.println("Class dropped.");
+            return true;
+        } else {
+            System.out.println("You have already used your drop option.");
+            return false;
+        }
     }
 
     // Allows the player to take an exam
     public Exam takeExam() {
         System.out.println("Starting exam.");
-        Exam exam = new Exam(/* Pass questions and timer values if needed */);
+        Exam exam = new Exam();
         exam.startExam();
         return exam;
+    }
+
+    // Allows the player to retake an exam if possible
+    public boolean retakeExam() {
+        if (canRetake) {
+            canRetake = false; // Set canRetake to false once used
+            System.out.println("Retaking the exam...");
+            Exam exam = new Exam();
+            exam.startExam();
+            return true;
+        } else {
+            System.out.println("You have already used your retake option.");
+            return false;
+        }
     }
 
     // Increment credits (could be called when passing an exam)
