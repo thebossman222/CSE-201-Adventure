f619a81f3ac54271884c4b1d2259ca646f8cedf2
mohamedlemin22 <155915386+mohamedlemin22@users.noreply.github.com>
2024-12-02 19:37:42 -0500

    Update Course.java

--- Course.java
+++ Course.java
@@ -2,7 +2,6 @@
 /**
  * @Class: Course
  * @authors: Caleb Krainman, Corbin Fulton, Andy Roberts, Mohamed Lemine E, Marissa Ellis, Ethan Jones
- * @version 1.0
  * @Written: 11/1/2024
  * @Course: CSE 201B: Intro to Software Engineering
  * @Purpose: The Class class represents a course in the MiamiQuest game, 
